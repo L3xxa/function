75211501cd10888f3a4009cd57f31aef35afca8a
Cycle <alekseiholovati@gmail.com>
2024-10-28 22:59:33 +0200

    complete ex_2

---
+++ function.py
@@ -0,0 +1,6 @@
+
+def spectrum (a,b):
+    for i in range(a,b+1):
+        if i % 2 == 0:
+            print(i)
+
---
+++ main.py
@@ -0,0 +1,6 @@
+from function import spectrum
+
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+
+spectrum(a,b)
