89e3ce513e77bc95991a4dc25fa2121d080e53bf
Cycle <alekseiholovati@gmail.com>
2024-10-29 00:22:59 +0200

    complete ex_4

---
+++ function.py
@@ -0,0 +1,10 @@
+
+def minimalne (a,b,c,d,e):
+    min = a
+    for i in a,b,c,d,e:
+        if i <min:
+            min = i
+    return min
+
+
+
---
+++ main.py
@@ -0,0 +1,6 @@
+from function import minimalne
+
+a,b,c,d,e = map(int,input("Через пробіл введіть пять чисел=>").split())
+
+result = minimalne(a,b,c,d,e)
+print(result)
