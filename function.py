065495048d51dfefd285aa2bf01c88ff7ea2f1f9
Cycle <alekseiholovati@gmail.com>
2024-10-29 13:00:52 +0200

    complete ex_6

---
+++ function.py
@@ -0,0 +1,8 @@
+
+def count_digits(a):
+    count = 0
+    for i in str(a):
+        count += 1
+    return count
+
+
---
+++ main.py
@@ -0,0 +1,6 @@
+from function import count_digits
+
+
+a = int(input("Enter a number: "))
+
+print(count_digits(a))
